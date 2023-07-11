0ec4403f414759d6f7584cdd4777841ec63a7181
Cseh Dániel <33789941+Cseh-Daniel@users.noreply.github.com>
2023-07-11 15:46:34 +0200

    preparing product card view

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { ref } from "vue";
 import Pagination from '@shared/Paginaton.vue';
+import ProductList from './Products/productList.vue';
+
 
 import { Link, usePage } from '@inertiajs/vue3';
 /* import the fontawesome core */
@@ -12,14 +14,16 @@ import { faImage } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faImage);
 
-let auth = ref(usePage().props.auth.user ? true:false);
+let auth = ref(usePage().props.auth.user ? true : false);
 
 </script>
 
 <template>
     <h1>Home</h1>
 
-    <div @logout="auth=false" class="card">
+    <ProductList></ProductList>
+
+    <div @logout="auth = false" class="card">
         <div class="card-body">
 
             <Link v-if="auth" href="/new-product" class="btn btn-outline-primary" as="button">New Product</Link>
@@ -46,8 +50,10 @@ let auth = ref(usePage().props.auth.user ? true:false);
                         <td>{{ product.price }}&nbsp;.-</td>
                         <td>
                             <div v-if="auth" class="d-flex gap-3 justify-content-center">
-                                <Link :href="'/edit-product/'+product.id" as="button" class="btn btn-outline-warning">Edit</Link>
-                                <Link :href="'/delete-product/'+product.id" as="button" class="btn btn-outline-danger" method="post" preserve-scroll>remove</Link>
+                                <Link :href="'/edit-product/' + product.id" as="button" class="btn btn-outline-warning">Edit
+                                </Link>
+                                <Link :href="'/delete-product/' + product.id" as="button" class="btn btn-outline-danger"
+                                    method="post" preserve-scroll>remove</Link>
                             </div>
                         </td>
 
---
+++ resources/js/Pages/Products/ProductList.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+let props = defineProps({
+    colNumber:{
+        type:Number,
+        default:3
+    },
+    items:Array
+})
+
+let arr = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'];
+
+let gr = [];
+
+let j = -1;
+
+for (let i = 0; i < arr.length; i++) {
+    if (i % props.colNumber == 0) {
+        j++;
+        gr[j] = [];
+    }
+    gr[j].push(arr[i]);
+}
+
+let header = `<h1>alma</h1>`;
+
+let cell = `    <div class="card">
+                    <div class="card-body">
+                        <h2>alma</h2>
+                    </div>
+                </div>`;
+</script>
+
+<template>
+    <div class="container text-center">
+
+        <div v-for="i in gr" :key="i" class="row my-3">
+
+            <div class="col" v-for="j in i" :key="j" v-html="cell">
+            </div>
+
+        </div>
+
+    </div>
+</template>
