251cfe9fe4357c0804709f1a90c3c9abf34d0246
Cseh Dániel <33789941+Cseh-Daniel@users.noreply.github.com>
2023-07-14 12:37:13 +0200

    before cart save

--- resources/js/Pages/Cart/CartItems.vue
+++ resources/js/Pages/Cart/CartItems.vue
@@ -6,7 +6,7 @@ defineProps({
 </script>
 
 <template>
-    <div class="row py-2 align-items-center justify-content-center gap-2">
+    <div class="row ps-3 py-2 align-items-center gap-2">
         <div class="col-2">{{ items.name }}</div>
         <div class="col-1 pe-5">{{ items.price }}.-</div>
         <div class="col-4 d-flex gap-1 align-items-center">Quantity:<input type="number" class="form-control"></div>
