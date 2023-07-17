c66b1ee39ced5cf8f39d3cee34c4ca01fae3ad04
Cseh Dániel <33789941+Cseh-Daniel@users.noreply.github.com>
2023-07-17 11:08:52 +0200

    register page
    Layout null
    Rearranged into card, card-body

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,16 +1,14 @@
 <script setup>
-import { useForm,Link } from '@inertiajs/vue3';
-
+import { useForm, Link } from '@inertiajs/vue3';
 
 let form = useForm({
     email: '',
     password: ''
 })
 
-function submit(){
+function submit() {
     form.post("/login");
 }
-
 
 </script>
 
@@ -21,37 +19,35 @@ export default {
 </script>
 
 <template>
+    <Head title="Login Page"></Head>
 
-        <Head title="Login Page"></Head>
+    <div class="border w-75 mx-auto p-5 mt-5 shadow rounded-4">
+        <h1>Login</h1>
+        <form @submit.prevent="submit" class="w-75 mx-auto mt-4">
 
-        <div class="border w-75 mx-auto p-5 mt-5 shadow rounded-4">
-            <h1>Login</h1>
-            <form @submit.prevent="submit" class="w-75 mx-auto mt-4">
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail:</label>
+                <input v-model="form.email" type="text" class="form-control" name="email" id="email" required>
+                <div v-if="form.errors.email" class="w-auto m-2">
+                    <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.email"></div>
+                </div>
+            </div>
 
-                <div class="mb-3">
-                    <label for="email" class="form-label">E-mail:</label>
-                    <input v-model="form.email" type="text" class="form-control" name="email" id="email" required>
-                    <div v-if="form.errors.email" class="w-auto m-2">
-                        <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.email"></div>
-                    </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password:</label>
+                <input v-model="form.password" type="password" class="form-control" name="password" id="password">
+
+                <div v-if="form.errors.password" class="w-auto m-2">
+                    <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.password"></div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="password" class="form-label">Password:</label>
-                    <input v-model="form.password" type="password" class="form-control" name="password" id="password">
+            </div>
 
-                    <div v-if="form.errors.password" class="w-auto m-2">
-                        <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.password"></div>
-                    </div>
+            <div class="d-flex gap-3 align-items-center">
+                <button class="btn btn-primary" type="submit">Login</button>
+                <Link href="/register" as="a">Don't have an account?</Link>
+            </div>
+        </form>
 
-                </div>
-
-                <div class="d-flex gap-3 align-items-center">
-                    <button class="btn btn-primary" type="submit">Login</button>
-                    <Link href="/register" as="a">Don't have an account?</Link>
-                </div>
-            </form>
-
-        </div>
-
+    </div>
 </template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,47 +1,6 @@
-<template>
-    <Head title="Create User"></Head>
-    <h1>Create New User</h1>
-
-    <form @submit.prevent="submit" class="w-75 mx-auto mt-4">
-
-        <div class="mb-3">
-            <label for="name" class="form-label rounded">Name:</label>
-            <input v-model="form.name" type="text" class="form-control" name="name" id="name" required>
-
-            <div v-if="form.errors.name" class="w-auto m-2">
-                <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.name"></div>
-            </div>
-
-        </div>
-
-
-        <div class="mb-3">
-            <label for="email" class="form-label">E-mail:</label>
-            <input v-model="form.email" type="text" class="form-control" name="email" id="email" required>
-            <div v-if="form.errors.email" class="w-auto m-2">
-                <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.email"></div>
-            </div>
-        </div>
-
-
-        <div class="mb-3">
-            <label for="password" class="form-label">Password:</label>
-            <input v-model="form.password" type="password" class="form-control" name="password" id="password">
-            <div v-if="form.errors.password" class="w-auto m-2">
-                <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.password"></div>
-            </div>
-        </div>
-
-<div class="d-flex gap-3 align-items-center">
-            <button class="btn btn-primary" type="submit" :disabled="form.processing">Register</button>
-            Already have an account?<Link href="/login" as="a">Login</Link>
-</div>
-
-    </form>
-</template>
 
 <script setup>
-import {useForm} from '@inertiajs/vue3';
+import { useForm } from '@inertiajs/vue3';
 
 let form = useForm({
     name: '',
@@ -55,3 +14,58 @@ function submit() {
 }
 
 </script>
+
+<script>
+export default {
+    layout: null
+}
+</script>
+
+<template>
+    <Head title="Create User"></Head>
+
+    <div class="card w-75 m-5 p-5 mx-auto shadow">
+        <h1>Create New User</h1>
+
+        <div class="card-body">
+            <form @submit.prevent="submit" class="w-75 mx-auto mt-4">
+
+                <div class="mb-3">
+                    <label for="name" class="form-label rounded">Name:</label>
+                    <input v-model="form.name" type="text" class="form-control" name="name" id="name" required>
+
+                    <div v-if="form.errors.name" class="w-auto m-2">
+                        <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.name"></div>
+                    </div>
+
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="email" class="form-label">E-mail:</label>
+                    <input v-model="form.email" type="text" class="form-control" name="email" id="email" required>
+                    <div v-if="form.errors.email" class="w-auto m-2">
+                        <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.email"></div>
+                    </div>
+                </div>
+
+
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password:</label>
+                    <input v-model="form.password" type="password" class="form-control" name="password" id="password">
+                    <div v-if="form.errors.password" class="w-auto m-2">
+                        <div class="p-2 badge bg-danger-subtle text-danger-emphasis" v-text="form.errors.password"></div>
+                    </div>
+                </div>
+
+                <div class="d-flex gap-3 align-items-center">
+                    <button class="btn btn-primary" type="submit" :disabled="form.processing">Register</button>
+                    Already have an account?
+                    <Link href="/login" as="a">Login</Link>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</template>
+
